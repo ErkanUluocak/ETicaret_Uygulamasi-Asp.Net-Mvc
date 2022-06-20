28cf6762d7fc8023286ed893cbc8701b2c2e9e59
Erkan Uluocak <erkan.uluocak@gmail.com>
2022-06-20 17:39:10 +0300

    web api

---
+++ WebAPIProject/Controllers/KategorilerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIProject.Models;
+
+namespace WebAPIProject.Controllers
+{
+    public class KategorilerController : ApiController
+    {
+        E_TicaretEntities db = new E_TicaretEntities();
+        public List<Kategorim> Get()
+        {
+
+            List<Kategori> kategoriler = db.Kategori.ToList();
+
+            List<Kategorim> liste = new List<Kategorim>();
+
+            foreach (var kategori in kategoriler)
+            {
+                liste.Add(new Kategorim() { ID = kategori.KategoriID, Name = kategori.KategoriAdi });
+            }
+
+            return liste;
+        }
+    }
+}
